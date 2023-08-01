0fd31ac5db4c5d58e83bd56c90c7535a53b99cce
Levis Matongo <Levismatongo@gmail.com>
2023-08-01 17:43:39 +0300

    first

---
+++ Invistico.R
@@ -0,0 +1,4 @@
+getwd()
+Invistico<-read.csv("Invistico_Airline.csv") #read the data set into R environment
+head(Invistico) #list first 6 rows of the data set
+str(Invistico) #Check the structure of the data
